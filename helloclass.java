96c34e7650d62a09781bd2197b243daeb0efeb4c
B21 <birgulgencustuner@gmail.com>
2021-12-17 22:18:21 +0000

    new commit added

--- helloclass.java
+++ helloclass.java
@@ -2,5 +2,6 @@ public class helloclass {
     public static void main(String[] args) {
         System.out.println("");
         ali ahmet
+        System.out.println("local repo is already published in github");
     }
 }
